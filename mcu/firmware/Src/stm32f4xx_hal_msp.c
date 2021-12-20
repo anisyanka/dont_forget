6b90963238d1a609b0cc87f571d6dbb8c6ed64a8
Aleksandr Anisimov <anisimov.alexander.s@gmail.com>
2021-12-20 23:00:21 +0300

    [mcu] Convert spaces to tabs for more convenient reading

--- mcu/firmware/Src/stm32f4xx_hal_msp.c
+++ mcu/firmware/Src/stm32f4xx_hal_msp.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-  * Initializes the Global MSP.
-  */
+	* Initializes the Global MSP.
+	*/
 void HAL_MspInit(void)
 {
-  __HAL_RCC_SYSCFG_CLK_ENABLE();
-  __HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_RCC_SYSCFG_CLK_ENABLE();
+	__HAL_RCC_PWR_CLK_ENABLE();
 
-  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
+	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
 }
 
 /**
@@ -19,27 +19,27 @@ void HAL_MspInit(void)
 */
 void HAL_UART_MspInit(UART_HandleTypeDef* huart)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  if(huart->Instance==USART2) {
-    /* Peripheral clock enable */
-    __HAL_RCC_USART2_CLK_ENABLE();
+	if(huart->Instance==USART2) {
+		/* Peripheral clock enable */
+		__HAL_RCC_USART2_CLK_ENABLE();
 
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    /**USART2 GPIO Configuration
-    PA2     ------> USART2_TX
-    PA3     ------> USART2_RX
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+		__HAL_RCC_GPIOA_CLK_ENABLE();
+		/**USART2 GPIO Configuration
+		PA2     ------> USART2_TX
+		PA3     ------> USART2_RX
+		*/
+		GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+		GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(USART2_IRQn);
-  }
+		HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
+		HAL_NVIC_EnableIRQ(USART2_IRQn);
+	}
 }
 
 /**
@@ -50,16 +50,16 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
 */
 void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
 {
-  if(huart->Instance==USART2) {
-    /* Peripheral clock disable */
-    __HAL_RCC_USART2_CLK_DISABLE();
+	if(huart->Instance==USART2) {
+		/* Peripheral clock disable */
+		__HAL_RCC_USART2_CLK_DISABLE();
 
-    /**USART2 GPIO Configuration
-    PA2     ------> USART2_TX
-    PA3     ------> USART2_RX
-    */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2|GPIO_PIN_3);
+		/**USART2 GPIO Configuration
+		PA2     ------> USART2_TX
+		PA3     ------> USART2_RX
+		*/
+		HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2|GPIO_PIN_3);
 
-    HAL_NVIC_DisableIRQ(USART2_IRQn);
-  }
+		HAL_NVIC_DisableIRQ(USART2_IRQn);
+	}
 }
--- mcu/firmware/Src/stm32f4xx_it.c
+++ mcu/firmware/Src/stm32f4xx_it.c
@@ -8,82 +8,82 @@ extern UART_HandleTypeDef huart2;
 /*           Cortex-M4 Processor Interruption and Exception Handlers          */
 /******************************************************************************/
 /**
-  * @brief This function handles Non maskable interrupt.
-  */
+	* @brief This function handles Non maskable interrupt.
+	*/
 void NMI_Handler(void)
 {
-  while (1)
-  {
-  }
+	while (1)
+	{
+	}
 }
 
 /**
-  * @brief This function handles Hard fault interrupt.
-  */
+	* @brief This function handles Hard fault interrupt.
+	*/
 void HardFault_Handler(void)
 {
-  while (1)
-  {
-  }
+	while (1)
+	{
+	}
 }
 
 /**
-  * @brief This function handles Memory management fault.
-  */
+	* @brief This function handles Memory management fault.
+	*/
 void MemManage_Handler(void)
 {
-  while (1)
-  {
-  }
+	while (1)
+	{
+	}
 }
 
 /**
-  * @brief This function handles Pre-fetch fault, memory access fault.
-  */
+	* @brief This function handles Pre-fetch fault, memory access fault.
+	*/
 void BusFault_Handler(void)
 {
-  while (1)
-  {
-  }
+	while (1)
+	{
+	}
 }
 
 /**
-  * @brief This function handles Undefined instruction or illegal state.
-  */
+	* @brief This function handles Undefined instruction or illegal state.
+	*/
 void UsageFault_Handler(void)
 {
-  while (1)
-  {
-  }
+	while (1)
+	{
+	}
 }
 
 /**
-  * @brief This function handles System service call via SWI instruction.
-  */
+	* @brief This function handles System service call via SWI instruction.
+	*/
 void SVC_Handler(void)
 {
 }
 
 /**
-  * @brief This function handles Debug monitor.
-  */
+	* @brief This function handles Debug monitor.
+	*/
 void DebugMon_Handler(void)
 {
 }
 
 /**
-  * @brief This function handles Pendable request for system service.
-  */
+	* @brief This function handles Pendable request for system service.
+	*/
 void PendSV_Handler(void)
 {
 }
 
 /**
-  * @brief This function handles System tick timer.
-  */
+	* @brief This function handles System tick timer.
+	*/
 void SysTick_Handler(void)
 {
-  HAL_IncTick();
+	HAL_IncTick();
 }
 
 /******************************************************************************/
@@ -95,5 +95,5 @@ void SysTick_Handler(void)
 
 void USART2_IRQHandler(void)
 {
-  HAL_UART_IRQHandler(&huart2);
+	HAL_UART_IRQHandler(&huart2);
 }
